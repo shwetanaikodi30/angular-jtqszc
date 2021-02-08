b4dbd4c24715a765695db7a29073bcad4d9dc79b
shwetanaikodi30 <naikodishweta@gmail.com>
2021-02-08 20:25:30 +0530

    testing first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
+  //committted
 })
 export class AppComponent  {
   name = 'Angular';
